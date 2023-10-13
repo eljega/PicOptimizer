f61bfe496b4a0e3be0576ea4935a17fcd36964c4
Javier <123848754+eljega@users.noreply.github.com>
2023-10-13 15:33:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import os
 
 
 
+
 app = Flask(__name__, static_url_path='/static')
+
+from PIL import Image
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_and_process_image():
@@ -27,6 +30,10 @@ def upload_and_process_image():
         if file:
             # Cargar la imagen original inmediatamente al recibirla
             img = Image.open(file)
+
+            # Convertir la imagen a modo RGBA para manejar la transparencia
+            img = img.convert('RGBA')
+
             original_buffer = io.BytesIO()
             img.save(original_buffer, format='PNG')
             original_image = base64.b64encode(original_buffer.getvalue()).decode()
@@ -37,6 +44,11 @@ def upload_and_process_image():
 
             # Guardar la imagen procesada en memoria
             output_buffer = io.BytesIO()
+
+            if output_format == 'jpeg':
+                # Si el formato de salida es JPEG, convierte a modo RGB antes de guardar
+                img = img.convert('RGB')
+
             img.save(output_buffer, output_format, quality=quality)
 
             # Convertir la imagen a base64 para mostrarla en la página
@@ -47,6 +59,7 @@ def upload_and_process_image():
             download_url = f"data:image/{output_format};base64,{processed_image}"
 
     return render_template('upload.html', original_image=original_image, processed_image=processed_image, processed_format=processed_format, download_url=download_url)
+
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
