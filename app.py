f16d3b15521b860b723d8362025413fd719eb978
Javier <123848754+eljega@users.noreply.github.com>
2023-10-13 09:08:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from PIL import Image
 import io
 import base64
 import os
+from rembg.cli import main
+
 
 app = Flask(__name__)
 
@@ -46,6 +48,5 @@ def upload_and_process_image():
 
     return render_template('upload.html', original_image=original_image, processed_image=processed_image, processed_format=processed_format, download_url=download_url)
 
-port = int(os.environ.get('PORT', 8080))
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', False))
+if __name__ == "__main__":
+    main()
