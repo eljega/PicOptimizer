5c0a6d267138192e015904da3a4d26ebdc05ddea
Javier <123848754+eljega@users.noreply.github.com>
2023-10-29 15:06:40 -0400

    Update waitress_config.py

--- waitress_config.py
+++ waitress_config.py
@@ -1,6 +1,5 @@
-# gunicorn_config.py
 from waitress import serve
-from app import app  # Asegúrate de que app importa tu aplicación Flask
+from app import app 
 
 
 serve(app, host="0.0.0.0", port=8080)
