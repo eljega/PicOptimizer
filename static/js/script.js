9253781f020b3cd3ebe49bb00f5a43fc479ce4be
Javier <123848754+eljega@users.noreply.github.com>
2023-10-29 15:09:04 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const modifiedImageContainer = document.getElementById('modifiedImageContainer');
     const modifiedImage = document.getElementById('modifiedImage');
 
-    // Función para cargar la imagen seleccionada por el usuario
+  
     fileInput.addEventListener('change', function (event) {
         const file = event.target.files[0];
         const reader = new FileReader();
@@ -21,18 +21,16 @@ document.addEventListener('DOMContentLoaded', function () {
         reader.readAsDataURL(file);
     });
 
-    // Función para convertir la imagen
+   
     convertButton.addEventListener('click', function () {
-        // Implementa la lógica para la conversión de imágenes aquí
-        // Asegúrate de mostrar la imagen modificada y el enlace de descarga
-        // después de la conversión.
+
         modifiedImageContainer.style.display = 'block';
         downloadLink.style.display = 'block';
     });
 
-    // Función para actualizar el enlace de descarga
+ 
     downloadLink.addEventListener('click', function () {
-        // Implementa la lógica para la descarga de la imagen modificada aquí
+   
     });
 });
 
